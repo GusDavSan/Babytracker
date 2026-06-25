789f41f152052c986e09daf7fa31fae721ab09aa
GusDavSan <GusDavSan@users.noreply.github.com>
2026-06-25 18:25:34 +0200

    Configure Firebase project

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyDxuVnknZDKj1KVZacCod18s7sUnT4AH5E",
+  authDomain: "babytracker-7a10f.firebaseapp.com",
+  projectId: "babytracker-7a10f",
+  storageBucket: "babytracker-7a10f.firebasestorage.app",
+  messagingSenderId: "795898398837",
+  appId: "1:795898398837:web:6aede9ca1d0b727df7d03c"
 };
 
 export const appSettings = {
